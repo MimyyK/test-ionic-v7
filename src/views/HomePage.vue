faf669774d9affae843a2168df2e6187e3da4191
MimyyK <michele.marais@hakisa.com>
2023-01-30 14:54:53 +0100

    Test v-model

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -14,15 +14,29 @@
       </ion-header>
 
       <div id="container">
-        <strong>Ready to create an app?</strong>
-        <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
+        <ion-list>
+          <ion-item>
+            <ion-input type="text" v-model="myField" label="My field v-model"></ion-input>
+          </ion-item>
+          <ion-item>
+            <ion-input type="text" :value="myFieldValueOnInput" label="My field value+onInput" @input="event => myFieldValueOnInput = event.target.value"></ion-input>
+          </ion-item>
+          <ion-item>
+            <ion-input type="text" :value="myFieldValueOnIonInput" label="My field value+onIonInput" @ion-input="event => myFieldValueOnIonInput = event.target.value"></ion-input>
+          </ion-item>
+        </ion-list>
       </div>
     </ion-content>
   </ion-page>
 </template>
 
 <script setup lang="ts">
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
+import { ref } from 'vue';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonInput, IonList, IonItem } from '@ionic/vue';
+
+const myField = ref('')
+const myFieldValueOnInput = ref('')
+const myFieldValueOnIonInput = ref('')
 </script>
 
 <style scoped>
