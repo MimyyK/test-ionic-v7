bba69d9cb889907d620da6dffbeaf47e87168485
MimyyK <michele.marais@hakisa.com>
2023-01-30 09:35:29 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'myAppV7',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
